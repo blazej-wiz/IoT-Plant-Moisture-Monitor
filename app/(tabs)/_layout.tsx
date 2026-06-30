5805506b2cea83fbc25b1f9bb73eee9d63c3702d
Blazej Jurczyk <bxj217@student.bham.ac.uk>
2026-06-30 22:01:41 +0100

    Rebuilt App Structure

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,39 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+
+
+export default function TabsLayout() {
+  return (
+  <Tabs
+    screenOptions={{
+        tabBarActiveTintColor: '#ffffff',
+        headerStyle: {
+            backgroundColor: '#ffffff'
+        },
+        headerShadowVisible: false,
+        headerTintColor: '#000000',
+        tabBarStyle: {
+            backgroundColor: '#ffffff',
+        },
+    }}
+  >
+    <Tabs.Screen 
+    name="index" 
+    options={{
+      headerTitle: 'Plant App',
+      tabBarIcon: ({focused, color}) => <Ionicons
+       name={focused ? "home-sharp" : "home-outline"}
+        size={24} 
+        color="black" />
+    }}/>
+
+    <Tabs.Screen name="about" options={{
+      headerTitle: 'About',
+      tabBarIcon: ({focused, color}) => <Ionicons
+       name={focused ? "information-circle-sharp" : "information-circle-outline"}
+        size={24} 
+        color="black" />
+    }} />
+  </Tabs>
+  );
+}
---
+++ app/(tabs)/about.tsx
@@ -0,0 +1,19 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function aboutssss() {
+  return (
+    <View
+      style={styles.container}>
+      <Text>About Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#ffffff',
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
