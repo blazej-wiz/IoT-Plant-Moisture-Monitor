888f257b0731d458fc882c4f8554fd1a4aa07c5d
Blazej Jurczyk <bxj217@student.bham.ac.uk>
2026-07-10 22:31:57 +0100

    setup initial NimBLE startup skeleton

--- hardware/sample_project/main/main.c
+++ hardware/sample_project/main/main.c
@@ -1,6 +1,85 @@
-#include <stdio.h>
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "nimble/nimble_port.h"
+#include "nimble/nimble_port_freertos.h"
+#include "host/ble_hs.h"
+
+#include "gap.h"
+
+#define TAG "PlantSensor"
+
+
+
+
+
+static void on_stack_reset(int reason) {
+    /* if nimble stack fails / resets then raise the warning */
+    ESP_LOGI(TAG, "nimble stack reset, reset reason %d", reason);
+}
+
+static void on_stack_sync(void) {
+    /* when nimble stack works, begin BLE advertising initialising*/
+    adv_init();
+}
+
+
+static void nimble_host_config_init(void) {
+    /* configures where the host should go if an event happens*/
+    ble_hs_cfg.reset_cb = on_stack_reset;
+    ble_hs_cfg.sync_cb = on_stack_sync;
+}
+
+
+/* begins the nimble BLE background service*/
+static void nimble_host_task(void *param) {
+    ESP_LOGI(TAG, "nimble host has been started");
+    nimble_port_run();
+    /* deletes the currently running task on FreeRTOS*/
+    vTaskDelete(NULL);
+}
+
 
 void app_main(void)
 {
-    printf("Hello world!\n");
+    /* aims to setup the ESP and services required for BLE and raises errors if one occurs*/
+    esp_err_t boot_storage = ESP_OK;
+    esp_err_t nimble_status = ESP_OK;
+    int gap_status = 0;
+
+
+
+    boot_storage = nvs_flash_init();
+    if (boot_storage == ESP_ERR_NVS_NO_FREE_PAGES ||
+    boot_storage == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        boot_storage = nvs_flash_init();
+    }
+    if (boot_storage != ESP_OK) {
+        ESP_LOGE(TAG, "failed to initialise nvs flash, error code: %d ", boot_storage);
+        return;
+    }
+
+    nimble_status = nimble_port_init();
+    if (nimble_status != ESP_OK) {
+        ESP_LOGE(TAG, "failed to initialise nimbe stack, error code %d", nimble_status);
+        return;
+    }
+    /* gap_init is not currently a function, that will need to be created to initialise gap server in different file later*/
+    gap_status = gap_init();
+    if (gap_status != 0) {
+        ESP_LOGE(TAG, "failed to initialise GAP server, error code %d", gap_status);
+        return;
+    }
+    /* initialises the callbacks for nimble to use, so when a certain event is triggered, nimble will refer to a specific function and execute it*/
+    nimble_host_config_init();
+
+    /* starts the BLE Nimble loop and keeps it running in the background on the FreeRTOS OS on the ESP32*/
+    xTaskCreate(nimble_host_task, "NimBLE Host", 4 * 1024, NULL, 5, NULL );
+    return;
+
 }
+
