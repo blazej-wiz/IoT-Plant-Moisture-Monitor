f281a96041457df4c194f31cd83b6661cae7f950
Blazej Jurczyk <bxj217@student.bham.ac.uk>
2026-07-26 15:58:49 +0100

    added delay to disconnect BLE

--- hardware/sample_project/main/src/wifi_setup.c
+++ hardware/sample_project/main/src/wifi_setup.c
@@ -20,6 +20,7 @@ static void wifi_event_handler(void *arg, esp_event_base_t event_base, int32_t e
     if(event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP){
         ESP_LOGE(TAG, "Wi-Fi connected");
         gatt_svc_set_setup_status("wifi_connected");
+        vTaskDelay(pdMS_TO_TICKS(5000));
         stop_advertising();
 }
 }
